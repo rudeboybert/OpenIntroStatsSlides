9674abc81cdc423ee2a1bcab7d21e2555a825dde
Albert Y. Kim <albert.ys.kim@gmail.com>
2014-10-01 10:13:11 -0700

    Done Lec13, started Lec14 Hypothesis Testing.

--- Lec13 CLT + Confidence Intervals/CLT.R
+++ Lec13 CLT + Confidence Intervals/CLT.R
@@ -1,4 +1,4 @@
-# setwd("./Lec13 Confidence Intervals/figure")
+# setwd("./Lec13 CLT + Confidence Intervals/figure/")
 
 #-------------------------------------------------------------------------------
 # Bimodal distribution of dragon wing spans:
@@ -15,10 +15,10 @@ y <- dnorm(x, mean=mu.1)*p.1 + dnorm(x, mean=mu.2)*p.2
 
 # Sampling distribution values
 n <- 15
-x.bars <- 
-  replicate(10000, 
+x.bars <-
+  replicate(10000,
             mean(
-              rnorm(n, 
+              rnorm(n,
                     mean=sample(c(mu.1, mu.2), n, replace=TRUE, prob=c(p.1, p.2))
               )
             )
@@ -42,8 +42,8 @@ plot(x, y, type='l', xlab="x: wing span (in feet)", ylab="",
 abline(v=mu, col="red", lwd=2)
 
 # Sampling Distribution
-hist(x.bars, xlim=range(x), 
-     xlab=expression(paste(bar(x), ": avg wing span (in feet)")), 
+hist(x.bars, xlim=range(x),
+     xlab=expression(paste(bar(x), ": avg wing span (in feet)")),
      prob=TRUE, ylab="",
      main="Sampling Dist'n")
 abline(v=mu, col="red", lwd=2)
@@ -64,7 +64,7 @@ breaks <- seq(0, 5, by=0.0005)
 # Population Distribution
 pdf("true.pdf", width=8*0.8, height=6*0.8)
 x <- seq(0, x.max, by=0.0001)
-plot(x, dlnorm(x, meanlog = meanlog, sdlog=sdlog), xlab="x", ylab="", 
+plot(x, dlnorm(x, meanlog = meanlog, sdlog=sdlog), xlab="x", ylab="",
      xlim=c(0, x.max), type='l')
 abline(v=exp(meanlog + sdlog^2/2), col="red", lwd=2)
 dev.off()
@@ -72,7 +72,7 @@ dev.off()
 for(n in c(2, 10, 30, 75)) {
   sim.means <- replicate(10000, mean(rlnorm(n, meanlog = meanlog, sdlog=sdlog)))
   pdf(paste("hist", n, ".pdf", sep=""), width=8*0.8, height=6*0.9)
-  hist(sim.means, main="", xlab=expression(bar(x)), xlim=c(0, x.max), 
+  hist(sim.means, main="", xlab=expression(bar(x)), xlim=c(0, x.max),
        breaks = breaks, ylim=c(0, 1300))
   abline(v=exp(meanlog + sdlog^2/2), col="red", lwd=2)
   dev.off()
